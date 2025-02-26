109aec51fffc1ac15c6476aa53014f8d56225fb7
Michael Geljic <michaelgeljic@gmail.com>
2025-02-26 14:19:01 +0100

    Create SensorFactory class

---
+++ src/edu/rit/croatia/swen383/g3/sensor/SensorFactory.java
@@ -0,0 +1,25 @@
+package edu.rit.croatia.swen383.g3.sensor;
+import edu.rit.croatia.swen383.g3.util.*;
+
+/*
+ * SensorFactort us responsible for creating sensor instances dynamically
+ */
+public class SensorFactory {
+    /**
+     * Creates a Sensor based on the provided SensorType.
+     * @param type the type of sensor to create
+     * @return the corresponding sensor instance
+     * @throws IllegalArgumentException if the sensortype is unknown
+     */
+    public static Sensor createSensor(SensorType type) {
+        switch (type) {
+            case TEMPERATURE:
+                return new TemperatureSensor();
+            case PRESSURE:
+                return new PressureSensor();
+            default:
+                throw new IllegalArgumentException("Unknown sensor type:" + ' ' + type);
+        }
+    }
+    
+}
