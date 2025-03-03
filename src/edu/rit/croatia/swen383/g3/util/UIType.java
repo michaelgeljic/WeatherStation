3f248f8ed607cf8e129a461fb6cca5e626ba1238
Michael Geljic <michaelgeljic@gmail.com>
2025-03-03 21:52:49 +0100

    Refactored TextUI humidity display and added Javadoc comments.

--- src/edu/rit/croatia/swen383/g3/util/UIType.java
+++ src/edu/rit/croatia/swen383/g3/util/UIType.java
@@ -1,5 +1,17 @@
 package edu.rit.croatia.swen383.g3.util;
 
+/**
+ * Enum representing the available user interface types in the system.
+ */
 public enum UIType {
-    TEXT, SWING, JAVAFX, STAT, FORECAST;
+    /** Text-based UI type */
+    TEXT,
+    /** Swing-based UI type */
+    SWING,
+    /** JavaFX-based UI type */
+    JAVAFX,
+    /** UI type for displaying sensor statistics */
+    STAT,
+    /** UI type for displaying sensor forecasts */
+    FORECAST;
 }
